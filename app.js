4b09d3014b4ba946682be02d0fb87002acf0583b
unknown <den199757@gmail.com>
2014-11-13 23:36:16 +0600

    c

---
+++ app.js
@@ -0,0 +1,26 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Спелый
+ * Date: 26.09.14
+ * Time: 15:22
+ * To change this template use File | Settings | File Templates.
+ */
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(3010);
+
+app.get('/chat', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+});
